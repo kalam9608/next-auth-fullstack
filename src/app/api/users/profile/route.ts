f13f812d485d321113d8a31a2e5f35945237fe70
mdkalam <arsh9608raja@gmail.com>
2024-09-12 02:39:23 +0530

    profile

---
+++ src/app/api/users/profile/route.ts
@@ -0,0 +1,26 @@
+import { ConnectDb } from "@/dbConfig/dbConfig";
+import { getDataFromToken } from "@/hepers/getDataFromToken";
+import User from "@/modals/user.modal";
+import { NextRequest, NextResponse } from "next/server";
+
+ConnectDb();
+
+export async function POST(request: NextRequest) {
+  try {
+    const userId = getDataFromToken(request);
+
+    const user = await User.findById({ id: userId }).select("-password");
+
+    if (!user) {
+      return NextResponse.json({ error: "user not exist" }, { status: 400 });
+    }
+
+    return NextResponse.json({
+      message: "User created successfully",
+      success: true,
+      user,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
---
+++ src/hepers/getDataFromToken.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+export async function getDataFromToken(request: NextRequest) {
+  try {
+    const token = request.cookies.get("token")?.value || "";
+
+    const decodedTotken: any = jwt.verify(token, process.env.TOKEN_SECRET!);
+    return decodedTotken.id;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
