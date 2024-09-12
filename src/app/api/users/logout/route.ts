76940863b6c61d1b1d8ed1cc8fd4b3c8b351d4c8
mdkalam <arsh9608raja@gmail.com>
2024-09-12 13:56:07 +0530

    backend done

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -11,7 +11,7 @@ export async function GET(request: NextRequest) {
       success: true,
     });
 
-    response.cookies.set("token", "", { httpOnly: true });
+    response.cookies.set("token", "", { httpOnly: true, expires: Date.now() });
 
     return response;
   } catch (error: any) {
